3c84baee63f8d7a462d5304e8c598d18ddec714c
HGray-EA <157797863+HGray-EA@users.noreply.github.com>
2025-04-21 17:18:23 +0000

    Missing pdf import request

--- Streamlit_bike_polo_app.py
+++ Streamlit_bike_polo_app.py
@@ -4,7 +4,7 @@ from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.llms import HuggingFaceEndpoint 
 from langchain.chains import RetrievalQA
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyMuPDFLoader
 from langchain.prompts import PromptTemplate
 import os
 import base64
